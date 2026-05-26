8b7cd19d49a3c86e99d4d7c902df35365924f79a
Tobias Höflmeier <tobias.hoeflmeier@outlook.de>
2026-05-26 10:19:43 +0200

    Enum ShadowAbilityType Added

---
+++ Source/ShadowStep/Core/Enums/ShadowAbilityType.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Core Memory Entertainment GbR. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ShadowAbilityType.generated.h"
+
+/**
+ * 
+ */
+ UENUM(BlueprintType)
+enum class  EShadowAbilityType : uint8
+{
+	SAT_TP,
+	SAT_LIGHT,
+	SAT_STONE
+};
+
