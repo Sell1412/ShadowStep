b54e20c9fb44458578f91514f949d36ecdeb72a0
Tobias Höflmeier <tobias.hoeflmeier@outlook.de>
2026-07-15 12:28:40 +0200

    Build Fix

--- Source/ShadowStep/ShadowStep.Build.cs
+++ Source/ShadowStep/ShadowStep.Build.cs
@@ -8,9 +8,9 @@ public class ShadowStep : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" , "AIModule", "GameplayTags", "DeveloperSettings","AudioModulation","AudioMixer","PhysicsCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTags", "DeveloperSettings", "AudioModulation", "AudioMixer", "PhysicsCore","UMG" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] {});
 
 		//Uncomment if you are using Slate UI
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
--- Source/YATools/YATools.Build.cs
+++ Source/YATools/YATools.Build.cs
@@ -29,6 +29,7 @@ public class YATools : ModuleRules
             "BlueprintGraph",
             "ScriptableEditorWidgets",
             "EditorScriptingUtilities",
+            "DeveloperSettings"
         });
     }
 }
